c4af5f793f10bdd5df587d08a647e964cd87130c
Dima Grossman <dima@grossman.io>
2020-05-20 22:00:07 +0300

    Fix error validation on command helper

--- apps/api/src/app/shared/commands/command.helper.ts
+++ apps/api/src/app/shared/commands/command.helper.ts
@@ -1,10 +1,20 @@
 import { plainToClass } from 'class-transformer';
+import { validateSync } from 'class-validator';
 import { ClassType } from 'class-transformer/ClassTransformer';
+import { BadRequestException, flatten } from '@nestjs/common';
 
 export class CommandHelper {
   static create<T>(command: ClassType<T>, data: object): T {
-    return plainToClass<T, object>(command, {
+    const convertedObject = plainToClass<T, object>(command, {
       ...data,
     });
+
+    const errors = validateSync(convertedObject);
+    if (errors?.length) {
+      const mappedErrors = flatten(errors.map((item) => Object.values(item.constraints)));
+      throw new BadRequestException(mappedErrors);
+    }
+
+    return convertedObject;
   }
 }
